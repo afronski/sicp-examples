449ac4849b40be2e76499c91342cbef43f385a3a
Wojciech Gawronski <afronski@gmail.com>
2014-10-28 20:12:37 +0100

    [SICP] Example - count coin change from 1.2 (Clojure).

---
+++ clojure/count-coin-change.clj
@@ -0,0 +1,20 @@
+(defn first-denomization [ kinds-of-coins ]
+  (cond (= kinds-of-coins 1) 1
+        (= kinds-of-coins 2) 5
+        (= kinds-of-coins 3) 10
+        (= kinds-of-coins 4) 25
+        (= kinds-of-coins 5) 50))
+
+(defn cc [ amount kinds-of-coins ]
+  (cond (= amount 0) 1 
+        (or (< amount 0) (= kinds-of-coins 0)) 0
+        :else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomization kinds-of-coins))
+                     kinds-of-coins))))
+
+(defn count-change [ amount ]
+  (cc amount 5))
+
+(println (count-change 100))
