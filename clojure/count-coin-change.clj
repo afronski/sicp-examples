540288a70e749f1a77bc9197d70a6e0748c1ebcd
Wojciech Gawronski <afronski@gmail.com>
2015-08-11 17:43:33 +0200

    More idiomatic example of `first-denomization`.

--- clojure/count-coin-change.clj
+++ clojure/count-coin-change.clj
@@ -1,12 +1,13 @@
 (defn first-denomization [ kinds-of-coins ]
-  (cond (= kinds-of-coins 1) 1
-        (= kinds-of-coins 2) 5
-        (= kinds-of-coins 3) 10
-        (= kinds-of-coins 4) 25
-        (= kinds-of-coins 5) 50))
+  (condp = kinds-of-coins
+    1 1
+    2 5
+    3 10
+    4 25
+    5 50))
 
 (defn cc [ amount kinds-of-coins ]
-  (cond (= amount 0) 1 
+  (cond (= amount 0) 1
         (or (< amount 0) (= kinds-of-coins 0)) 0
         :else (+ (cc amount
                      (- kinds-of-coins 1))
